bfff709b4b237817762381c109c677feb2e308ff
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-10 17:21:32 +0000

    Setting up rest api endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,24 @@
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET'])
+def hello(request):
+    return Response({'res':'hello'})
 
-def hello(req):
-    return HttpResponse("Hello")
+@api_view(['GET'])
+def home(request):
+    return Response({'res':'hello'})
 
-def home(req):
-    return HttpResponse("Home")
-    
-def login(req):
-    return HttpResponse("login")
+@api_view(['GET'])
+def login(request):
+    return Response({'res':'hello'})
 
+@api_view(['GET'])
 def register(req):
-    return HttpResponse("register")
+    return Response({'res':'hello'})
+
+      
